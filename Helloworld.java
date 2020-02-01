31dbaa8ae8bbc32c4fd545fb91cde26af0d66fd8
Rei Silva <reisilva@Reis-MacBook-Pro.local>
2020-02-01 17:00:44 -0500

    changed 'Hello, stupid' to 'Hello, world, goteeem'

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ class HelloWorld
 {
     public static void main (String args[])
     {
-        System.out.println("Hello, stupid");
+        System.out.println("Hello, world, goteeem");
         
     }
 }
